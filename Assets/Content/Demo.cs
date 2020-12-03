55949c9ecad3662117b4545bc3b3c32c6c73f1df
AntonC9018 <anton.curm@gmail.com>
2020-12-03 18:42:10 +0200

    Refactor demo a little bit

--- Assets/Content/Demo.cs
+++ Assets/Content/Demo.cs
@@ -1,18 +1,17 @@
-﻿using UnityEngine;
-using Core;
+﻿using Core;
 using Core.Generation;
-using Core.Utils.Vector;
-using Core.Behaviors;
-using Core.Targeting;
-using System.Collections.Generic;
-using Core.Items;
-using System.Linq;
-using Core.Utils;
-using Test;
 using Core.History;
-using Hopper.ViewModel;
+using Core.Items;
+using Core.Targeting;
+using Core.Utils.Vector;
+
 using Hopper.View;
-using Core.Stats.Basic;
+using Hopper.ViewModel;
+
+using Test;
+
+using UnityEngine;
+
 
 namespace Hopper
 {
@@ -43,7 +42,110 @@ namespace Hopper
         private ModularWeapon m_knifeItem;
         private View_Model m_viewModel;
         private InputManager m_inputManager;
+        private Factories m_factories;
 
+        private void Update()
+        {
+            if (m_inputManager.TrySetAction(m_world))
+            {
+                m_world.Loop();
+            }
+        }
+
+        private void Start()
+        {
+            m_inputManager = new InputManager();
+
+            // Redirects System.Console.WriteLine to unity's console. By default, it goes to debug logs.
+            Core.Utils.UnitySystemConsoleRedirector.Redirect();
+
+            // Generates the map
+            var generator = CreateRunGenerator();
+
+            m_world = new World(generator.grid.GetLength(1), generator.grid.GetLength(0));
+
+            CreateItems();
+            m_factories = new Factories();
+            m_world.m_pools.UsePools(itemPool: CreateItemPool(), entityPool: new ThrowawayPool());
+
+            // Create view_model and hook it up to watch the world events
+            SetupViewModel(m_factories, m_world);
+
+            for (int y = 0; y < generator.grid.GetLength(1); y++)
+            {
+                for (int x = 0; x < generator.grid.GetLength(0); x++)
+                {
+                    if (generator.grid[x, y] != Generator.Mark.EMPTY)
+                    {
+                        TileStuff.FireCreatedEvent(new IntVector2(x, y));
+
+                        if (generator.grid[x, y] == Generator.Mark.WALL)
+                        {
+                            m_world.SpawnEntity(m_factories.wallFactory, new IntVector2(x, y));
+                        }
+                    }
+                }
+            }
+
+            var center = generator.rooms[0].Center.Round();
+
+            var player = m_world.SpawnPlayer(m_factories.playerFactory, center);
+
+            /* Bounce trap and a wall. */
+            // m_world.SpawnEntity(m_factories.trapFactory, center + IntVector2.Right, IntVector2.Right);
+            // m_world.SpawnEntity(m_factories.wallFactory, center + IntVector2.Right * 2);
+
+            /* Two bounce traps in a row. */
+            // m_world.SpawnEntity(m_factories.trapFactory, center + IntVector2.Right, IntVector2.Right);
+            // m_world.SpawnEntity(trapFactory, center + IntVector2.Right * 2, IntVector2.Left);
+
+            /* Uncomment to disable bouncing for player. */
+            // player.Stats.GetRaw(Push.Source.Resistance.Path)[BounceTrap.BounceSource.Id] = 3;
+
+            /* A blocking trap. When you step on it, it closes you in. */
+            // m_world.SpawnEntity(BlockingTrap.CreateFactory(), player.Pos + IntVector2.Right);
+
+            /* Knife and Shivel basic equipment. */
+            // player.Inventory.Equip(m_knifeItem);
+            // player.Inventory.Equip(shovelItem);
+
+            /* Bow. X toggle charge, vector input to shoot */
+            // player.Inventory.Equip(Bow.DefaultItem);
+
+            /* 10000 bombs. `Space` to use. */
+            // player.Inventory.Equip(new PackedItem(Bombing.item, 10000));
+
+            /* Knippers (explody boys). */
+            // m_world.SpawnEntity(Knipper.Factory, new IntVector2(center.x + 4, center.y));
+            // m_world.SpawnEntity(Knipper.Factory, new IntVector2(center.x + 3, center.y));
+
+            /* A test robot boss that spawns whelps behind itself and shoots lasers. */
+            // m_world.SpawnEntity(TestBoss.Factory, new IntVector2(center.x + 4, center.y));
+
+            /* A barrier block blocks movement only through one side. The second coordinate is the 
+               orientation, which for such blocks defines what side of the cell it is at.
+            */
+            // m_world.SpawnEntity(m_factories.barrierFactory,
+            //    new IntVector2(center.x + 2, center.y), new IntVector2(-1, 0));
+
+            /* A chest contains something depending on the factory. It may spawn preset entities,
+               items or draw them from specified in the config pools and gold.
+               See how the factory is defined.
+            */
+            // m_world.SpawnEntity(m_factories.chestFactory, new IntVector2(center.x + 1, center.y + 1));
+
+            /* Water blocks stop one movement, attack or dig. */
+            // m_world.SpawnEntity(m_factories.waterFactory, new IntVector2(center.x + 1, center.y + 1));
+            // m_world.SpawnEntity(m_factories.waterFactory, new IntVector2(center.x, center.y + 1));
+
+            /* Ice makes you slide, skipping movement, attack or dig. */
+            // m_world.SpawnEntity(m_factories.iceFactory, new IntVector2(center.x - 1, center.y - 1));
+            // m_world.SpawnEntity(m_factories.iceFactory, new IntVector2(center.x, center.y - 1));
+            // m_world.SpawnEntity(m_factories.iceFactory, new IntVector2(center.x + 1, center.y - 1));
+
+            /* Apply freezing on player. */
+            // FreezeStatus.Status.TryApply(player, new FreezeData(), FreezeStat.Path.DefaultFile);
+        }
 
         private ISuperPool CreateItemPool()
         {
@@ -62,57 +164,12 @@ namespace Hopper
             pool.Add("zone1/stuff", items[2]);
             // pool.Add("zone1/stuff", items[3]);
 
-            return pool.Copy();
+            return pool;
+            // return pool.Copy();
         }
 
-        void Start()
+        private void SetupViewModel(Factories factories, World world)
         {
-            m_inputManager = new InputManager();
-            Core.Utils.UnitySystemConsoleRedirector.Redirect();
-
-            CreateItems();
-
-            var itemPool = CreateItemPool();
-            var entityPool = new ThrowawayPool();
-
-            ContentProvider.DefaultProvider.UsePools(entityPool, itemPool);
-
-            var playerFactory = CreatePlayerFactory().AddBehavior<Statused>();
-
-            var wallFactory = new EntityFactory<Wall>()
-                .AddBehavior<Attackable>()
-                .Retouch(
-                    Core.Retouchers.Attackableness.Constant(AtkCondition.NEVER)
-                )
-                .AddBehavior<Damageable>();
-
-            var enemyFactory = Test.Skeleton.Factory;
-
-            var chestFactory = new EntityFactory<Entity>()
-                .AddBehavior<Interactable>(
-                    new Interactable.Config
-                    {
-                        contentConfig = new ContentConfig
-                        {
-                            type = ContentType.ITEM,
-                            isAforeset = false,
-                            poolPath = "zone1/stuff"
-                        }
-                    }
-                );
-
-            var waterFactory = Water.CreateFactory();
-            var iceFactory = IceFloor.CreateFactory();
-            var trapFactory = BounceTrap.Factory;
-            var barrierFactory = BlockingTrap.BarrierFactory;
-
-            var itemMap = Registry.Default.Items.PackModMap();
-            Registry.Default.Items.SetServerMap(itemMap);
-
-            var generator = CreateGenerator();
-            generator.Generate();
-            m_world = new World(generator.grid.GetLength(1), generator.grid.GetLength(0));
-
             var destroyOnDeathSieve = new SimpleSieve(AnimationCode.Destroy, UpdateCode.dead);
             var playerJumpSieve = new SimpleSieve(AnimationCode.Jump, UpdateCode.move_do);
 
@@ -121,25 +178,25 @@ namespace Hopper
 
             m_viewModel = new View_Model(animator);
             m_viewModel.SetDefaultPrefab(new Prefab<SceneEnt>(defaultPrefab, destroyOnDeathSieve));
-            m_viewModel.SetPrefabForFactory(playerFactory.Id,
+            m_viewModel.SetPrefabForFactory(factories.playerFactory.Id,
                 new Prefab<SceneEnt>(playerPrefab, destroyOnDeathSieve, playerJumpSieve));
-            m_viewModel.SetPrefabForFactory(enemyFactory.Id,
+            m_viewModel.SetPrefabForFactory(factories.enemyFactory.Id,
                 new Prefab<SceneEnt>(enemyPrefab, destroyOnDeathSieve));
-            m_viewModel.SetPrefabForFactory(wallFactory.Id,
+            m_viewModel.SetPrefabForFactory(factories.wallFactory.Id,
                 new Prefab<SceneEnt>(wallPrefab, destroyOnDeathSieve));
-            m_viewModel.SetPrefabForFactory(chestFactory.Id,
+            m_viewModel.SetPrefabForFactory(factories.chestFactory.Id,
                 new Prefab<SceneEnt>(chestPrefab, destroyOnDeathSieve));
             m_viewModel.SetPrefabForFactory(BombEntity.Factory.Id,
                 new Prefab<SceneEnt>(bombPrefab, destroyOnDeathSieve));
             m_viewModel.SetPrefabForFactory(DroppedItem.Factory.Id,
                 new Prefab<RegularRotationSceneEnt>(droppedItemPrefab, destroyOnDeathSieve));
-            m_viewModel.SetPrefabForFactory(waterFactory.Id,
+            m_viewModel.SetPrefabForFactory(factories.waterFactory.Id,
                 new Prefab<SceneEnt>(waterPrefab, destroyOnDeathSieve));
-            m_viewModel.SetPrefabForFactory(iceFactory.Id,
+            m_viewModel.SetPrefabForFactory(factories.iceFactory.Id,
                 new Prefab<SceneEnt>(icePrefab, destroyOnDeathSieve));
-            m_viewModel.SetPrefabForFactory(trapFactory.Id,
+            m_viewModel.SetPrefabForFactory(factories.trapFactory.Id,
                 new Prefab<SceneEnt>(bounceTrapPrefab, destroyOnDeathSieve));
-            m_viewModel.SetPrefabForFactory(barrierFactory.Id,
+            m_viewModel.SetPrefabForFactory(factories.barrierFactory.Id,
                 new Prefab<RegularRotationSceneEnt>(barrierPrefab, destroyOnDeathSieve));
             m_viewModel.SetPrefabForFactory(Knipper.Factory.Id,
                 new Prefab<SceneEnt>(knipperPrefab, destroyOnDeathSieve));
@@ -151,103 +208,10 @@ namespace Hopper
             var explosionWatcher = new ExplosionWatcher(explosionPrefab);
             var laserBeamWatcher = new LaserBeamWatcher(laserBeamHeadPrefab, laserBeamBodyPrefab);
             var tileWatcher = new TileWatcher(new Prefab<SceneEnt>(tilePrefab));
-            m_viewModel.WatchWorld(m_world, explosionWatcher, tileWatcher, laserBeamWatcher);
 
             Reference.Width = playerPrefab.GetComponent<SpriteRenderer>().size.x;
 
-            // m_candaceAnimationManager.SetWorld(m_world);
-
-            for (int y = 0; y < generator.grid.GetLength(1); y++)
-            {
-                for (int x = 0; x < generator.grid.GetLength(0); x++)
-                {
-                    if (generator.grid[x, y] != Generator.Mark.EMPTY)
-                    {
-                        TileStuff.FireCreatedEvent(new IntVector2(x, y));
-
-                        if (generator.grid[x, y] == Generator.Mark.WALL)
-                        {
-                            m_world.SpawnEntity(wallFactory, new IntVector2(x, y));
-                        }
-                    }
-                }
-            }
-
-            var center = generator.rooms[0].Center.Round();
-
-            var _ = Bow.DefaultItem; // this is required to reference the slot at least once
-            // player.Inventory.Equip(m_knifeItem);
-
-            var player = m_world.SpawnPlayer(playerFactory, center);
-            m_world.SpawnEntity(trapFactory, center + IntVector2.Right, IntVector2.Right);
-            // player.Stats.GetRaw(Push.Source.Resistance.Path)[BounceTrap.BounceSource.Id] = 20;
-            m_world.SpawnEntity(wallFactory, center + IntVector2.Right * 2);
-
-            // m_world.SpawnEntity(trapFactory, center + IntVector2.Right * 2, IntVector2.Left);
-
-            // m_world.SpawnEntity(BlockingTrap.CreateFactory(), player.Pos + IntVector2.Right);
-
-            // player.Inventory.Equip(Bow.DefaultItem);
-            // player.Inventory.Equip(m_knifeItem);
-            // player.Inventory.Equip(new PackedItem(Bombing.item, 10000));
-            // player.Inventory.Equip(shovelItem);
-            // player.Inventory.Equip(knifeItem);
-
-            // m_world.SpawnEntity(Knipper.Factory, new IntVector2(center.x + 4, center.y));
-            // m_world.SpawnEntity(Knipper.Factory, new IntVector2(center.x + 3, center.y));
-
-            // m_world.SpawnEntity(TestBoss.Factory, new IntVector2(center.x + 4, center.y));
-            // m_world.SpawnEntity(barrierFactory, new IntVector2(center.x + 2, center.y), new IntVector2(-1, 0));
-
-            // m_world.SpawnEntity(chestFactory, new IntVector2(center.x + 1, center.y + 1));
-            // m_world.SpawnEntity(waterFactory, new IntVector2(center.x + 1, center.y + 1));
-            // m_world.SpawnEntity(waterFactory, new IntVector2(center.x, center.y + 1));
-            // m_world.SpawnEntity(iceFactory, new IntVector2(center.x - 1, center.y - 1));
-            // m_world.SpawnEntity(iceFactory, new IntVector2(center.x, center.y - 1));
-            // m_world.SpawnEntity(iceFactory, new IntVector2(center.x + 1, center.y - 1));
-            // var trap0 = m_world.SpawnEntity(trapFactory, new IntVector2(center.x - 2, center.y - 1));
-            // trap0.Reorient(new IntVector2(1, 0));
-            // FreezeStatus.Status.TryApply(player, new FreezeData(), FreezeStat.Path.DefaultFile);
-        }
-
-        private void Update()
-        {
-            if (m_inputManager.TrySetAction(m_world))
-            {
-                m_world.Loop();
-            }
-        }
-
-        private EntityFactory<Player> CreatePlayerFactory()
-        {
-            var moveAction = new BehaviorAction<Moving>();
-            var digAction = new BehaviorAction<Digging>();
-            var attackAction = new BehaviorAction<Attacking>();
-            var interactAction = new SimpleAction(
-                (Entity actor, Action action) =>
-                {
-                    var target = actor.GetCellRelative(action.direction)?.GetAnyEntityFromLayer(Layer.REAL);
-                    if (target == null) return false;
-                    var interactable = target.Behaviors.TryGet<Interactable>();
-                    if (interactable == null) return false;
-                    return interactable.Activate();
-                }
-            );
-
-            var defaultAction = new CompositeAction(
-                interactAction,
-                attackAction,
-                digAction,
-                moveAction
-            );
-
-            var playerFactory = Player.CreateFactory()
-                .AddBehavior<Controllable>(new Controllable.Config
-                {
-                    defaultAction = defaultAction
-                });
-
-            return playerFactory;
+            m_viewModel.WatchWorld(world, explosionWatcher, tileWatcher, laserBeamWatcher);
         }
 
         private void CreateItems()
@@ -269,9 +233,12 @@ namespace Hopper
             m_knifeItem = new ModularWeapon(
                 knifeTargetProvider
             );
+
+            // Reference this item once so that it is added to the registry
+            var _ = Bow.DefaultItem;
         }
 
-        private Generator CreateGenerator()
+        private Generator CreateRunGenerator()
         {
             Generator generator = new Generator(11, 11, new Generator.Options
             {
@@ -284,7 +251,7 @@ namespace Hopper
             // generator.AddRoom(new IntVector2(5, 5));
             // generator.AddRoom(new IntVector2(5, 5));
             // generator.AddRoom(new IntVector2(5, 5));
-            // generator.Generate();
+            generator.Generate();
             return generator;
         }
     }
---
+++ Assets/Content/Factories.cs
@@ -0,0 +1,76 @@
+using Core;
+using Core.Behaviors;
+using Core.Items;
+using Core.Targeting;
+using Test;
+
+namespace Hopper
+{
+    public class Factories
+    {
+        public EntityFactory<Player> playerFactory;
+        public EntityFactory<Wall> wallFactory;
+        public EntityFactory<Entity> enemyFactory;
+        public EntityFactory<Entity> chestFactory;
+        public EntityFactory<Water> waterFactory;
+        public EntityFactory<IceFloor> iceFactory;
+        public EntityFactory<BounceTrap> trapFactory;
+        public EntityFactory<RealBarrier> barrierFactory;
+
+        public Factories()
+        {
+            playerFactory = CreatePlayerFactory().AddBehavior<Statused>();
+
+            wallFactory = new EntityFactory<Wall>()
+                .AddBehavior<Attackable>()
+                .Retouch(
+                    Core.Retouchers.Attackableness.Constant(AtkCondition.NEVER)
+                )
+                .AddBehavior<Damageable>();
+
+            enemyFactory = Test.Skeleton.Factory;
+
+            chestFactory = new EntityFactory<Entity>()
+                .AddBehavior<Interactable>(
+                    new Interactable.Config(new PoolItemContentSpec("zone1/stuff"))
+                );
+
+            waterFactory = Water.CreateFactory();
+            iceFactory = IceFloor.CreateFactory();
+            trapFactory = BounceTrap.Factory;
+            barrierFactory = BlockingTrap.BarrierFactory;
+        }
+
+        private EntityFactory<Player> CreatePlayerFactory()
+        {
+            var moveAction = new BehaviorAction<Moving>();
+            var digAction = new BehaviorAction<Digging>();
+            var attackAction = new BehaviorAction<Attacking>();
+            var interactAction = new SimpleAction(
+                (Entity actor, Action action) =>
+                {
+                    var target = actor.GetCellRelative(action.direction)?.GetAnyEntityFromLayer(Layer.REAL);
+                    if (target == null) return false;
+                    var interactable = target.Behaviors.TryGet<Interactable>();
+                    if (interactable == null) return false;
+                    return interactable.Activate();
+                }
+            );
+
+            var defaultAction = new CompositeAction(
+                interactAction,
+                attackAction,
+                digAction,
+                moveAction
+            );
+
+            var playerFactory = Player.CreateFactory()
+                .AddBehavior<Controllable>(new Controllable.Config
+                {
+                    defaultAction = defaultAction
+                });
+
+            return playerFactory;
+        }
+    }
+}
