c9cd2a8c0619020dc727fc332e1340f2788893e8
AntonC9018 <anton.curm@gmail.com>
2020-12-15 23:30:54 +0200

    remove isFirstTimeInPhase

--- Assets/ViewModel/Data/AnimationInfo.cs
+++ Assets/ViewModel/Data/AnimationInfo.cs
@@ -2,7 +2,6 @@ namespace Hopper.Controller
 {
     public class AnimationInfo
     {
-        public bool isFirstTimeInPhase;
         public int currentPhase;
         public float proportionIntoPhase;
         public int tickCount;
